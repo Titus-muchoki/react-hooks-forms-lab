d6d357d868e18a5fe82843e926ad79c14fd8372e
kajela <titoyut56@gmail.com>
2023-06-15 08:00:54 -0400

    add the initial files

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -22,15 +22,22 @@ function ItemForm({onItemFormSubmit}) {
     });
    }
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={handleSubmit}>
       <label>
         Name:
-        <input type="text" name="name" />
+        <input type="text"
+         name="name"
+         value={name}
+         onChange={handleNameChange}
+          />
       </label>
 
       <label>
         Category:
-        <select name="category">
+        <select name="category"
+          value={category}
+          onChange={handleCategoryChange}
+          >
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
